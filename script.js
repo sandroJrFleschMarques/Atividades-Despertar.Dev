44922ef6eac7c5e9527b6eb3b2b7438a228d774c
Sandro Junior <sandrojr998@gmail.com>
2023-10-21 23:47:19 -0300

    f2

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ $(document).on('keydown', function (e) {
     let up = 38;
     let down = 40;
     let enter = 13;
-    let esc = 27;
+    let f2 = 113;
 
     // VARIAVEIS AUXILIARES PARA NAVEGACAO
     let index = 1;
     let proximo = 1;
 
     // VERIFICA SE A TECLA QUE ESTÁ SENDO DISPARADA É ALGUMA TECLA QUE QUEREMOS TRABALHAR EM CIMA
-    if (e.keyCode === up || e.keyCode === down || e.keyCode === enter || e.keyCode === esc) {
+    if (e.keyCode === up || e.keyCode === down || e.keyCode === enter || e.keyCode === f2) {
 
         // PERCORRE TODAS AS DIVS DA CLASSE ITEM PARA SETAR O INDICE DA PROXIMA
         $.each($('.item'), function () {
@@ -31,7 +31,7 @@ $(document).on('keydown', function (e) {
                         $('.item').click();
                         window.location.href = this.href
                         break;
-                    case esc:
+                    case f2:
                         window.history.back();
                         break;
 
