b96038bf8cff50540ad9acf3eaef4086a76c4a5f
Sandro Junior <sandrojr998@gmail.com>
2023-10-25 22:42:08 -0300

    novo script

--- script.js
+++ script.js
@@ -1,61 +1,48 @@
-$(document).on('keydown', function (e) {
+let i = -1
+window.addEventListener('keydown', (event) => {
 
-    // ATRIBUINDO O KEY CODE PARA AS VARIAVEIS PARA MELHOR LEGIBILIDADE
-
-    let up = 38;
-    let down = 40;
-    let enter = 13;
-    let f2 = 113;
-
-    // VARIAVEIS AUXILIARES PARA NAVEGACAO
-    let index = 1;
-    let proximo = 1;
-
-    // VERIFICA SE A TECLA QUE ESTÁ SENDO DISPARADA É ALGUMA TECLA QUE QUEREMOS TRABALHAR EM CIMA
-    if (e.keyCode === up || e.keyCode === down || e.keyCode === enter || e.keyCode === f2) {
-
-        // PERCORRE TODAS AS DIVS DA CLASSE ITEM PARA SETAR O INDICE DA PROXIMA
-        $.each($('.item'), function () {
-            $(document).keypress(function (e) {
-
-            });
-            if ($(this).hasClass('selected')) {
-                switch (e.keyCode) {
-                    case up:
-                        proximo = index - 1
-                        break;
-                    case down:
-                        proximo += index
-                        break;
-                    case enter:
-                        $('.item').click();
-                        window.location.href = this.href
-                        break;
-                    case f2:
-                        window.history.back();
-                        break;
-
-                }
-            }
-            index++;
-        });
-
-        index = 1;
-        // VERIFICA SE O RETORNO É MAIOR QUE O NUMERO TOTAL DE DIVS E RETORNA FALSO PARA A NAVEGACAO NÃO SAIR DE DAS DIVS
-        if (proximo > $('.item').length) {
-            return false;
-            // VERIFICA SE O RETORNO É MENOR QUE 1 E RETORNA FALSO PARA A NAVEGAÇÃO NÃO SAIR DAS DIVS
-        } else if (proximo < 1) {
-            return false;
-        }
-        // PERCORRE TODAS AS DIVS ITEMS PARA ATRIBUIR A CLASSE SELECTED NA DIV QUE O CURSOR DEVE IR SETADO NA VARIAVEL PROXIMO
-        $.each($('.item'), function () {
-            $(this).removeClass('selected');
-            if (index === proximo) {
-                $(this).addClass('selected');
-            }
-            index++;
-        })
+    let sel = document.getElementsByClassName('item')
+    let proximo = 0
+    function tecla() {
+        i += 1
     }
+    let selL = sel.length
+    if (event.which === 40 && i < selL - 2) {
+        tecla()
+        if (sel[i].className === 'item selected') {
+            sel[i].classList.remove('selected')
+            proximo = i + 1
+            let sel2 = sel[proximo]
+            sel2.classList.add('selected')
+        }
+    }
+    function tecla2() {
+        i -= 1
+    }
+    if (event.which === 38 && i > -1) {
 
+        if (sel[i + 1].className === 'item selected') {
+            sel[i + 1].classList.remove('selected')
+            proximo = i
+            let sel2 = sel[proximo]
+            sel2.classList.add('selected')
+        }
+        tecla2()
+    }
+    if (event.which === 13) {
+        let ent = document.querySelector('.selected').click()
+        window.location.href = ent.this.href
+
+    }
+    if (event.which === 113) {
+        window.history.back()
+    }
 });
+
+
+
+
+
+
+
+
