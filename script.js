91fa501566d82cf14919320631ee0108486068e2
Sandro Junior <sandrojr998@gmail.com>
2023-10-27 19:05:17 -0300

    script

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
-let i = -1
+let i = 0
 window.addEventListener('keydown', (event) => {
 
     let sel = document.getElementsByClassName('item')
-    let proximo = 0
-    function tecla() {
-        i += 1
-    }
-    let selL = sel.length
-    if (event.which === 40 && i < selL - 2) {
-        tecla()
+    if (event.which === 40 && i < (sel.length - 1)) {
         if (sel[i].className === 'item selected') {
             sel[i].classList.remove('selected')
-            proximo = i + 1
-            let sel2 = sel[proximo]
-            sel2.classList.add('selected')
+            sel[i + 1].classList.add('selected')
         }
+        i++
     }
-    function tecla2() {
-        i -= 1
-    }
-    if (event.which === 38 && i > -1) {
-
-        if (sel[i + 1].className === 'item selected') {
-            sel[i + 1].classList.remove('selected')
-            proximo = i
-            let sel2 = sel[proximo]
-            sel2.classList.add('selected')
+    if (event.which === 38 && i > 0) {
+        if (sel[i].className === 'item selected') {
+            sel[i].classList.remove('selected')
+            sel[i - 1].classList.add('selected')
         }
-        tecla2()
+        i--
     }
     if (event.which === 13) {
         let ent = document.querySelector('.selected').click()
         window.location.href = ent.this.href
-
     }
     if (event.which === 113) {
         window.history.back()
