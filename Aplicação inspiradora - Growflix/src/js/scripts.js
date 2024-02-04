5b66b2cf60509563a5b93df0b36eca6053901e5f
Sandro Junior <sandrojr998@gmail.com>
2024-02-04 00:07:34 -0300

    apl

--- Aplicação inspiradora - Growflix/src/js/scripts.js
+++ Aplicação inspiradora - Growflix/src/js/scripts.js
@@ -3,20 +3,19 @@ const webinar = document.getElementById('webinar')
 const uxui = document.getElementById('ux-ui')
 const geral = document.getElementById('geral')
 
-function render(a, categ) {
+function render(categDom, categData) {
     for (let i = 0; i < movies.length; i++) {
-        if (movies[i].category == categ) {
-            a.innerHTML += `
+        if (movies[i].category == categData) {
+            categDom.innerHTML += `
             
-    <div class="bg-black p-2 col-8 offset-2 col-md-3 offset-md-0 border border-0">
-        <div class="cardZ" onmouseover="addd(${i})">
+    <div class="bg-black p-2 col-12 col-md-3 offset-md-0 border border-0">
+        <div class="cardZ" onmouseover="adicionarClasse(${i})" onclick="modal(${i})">
             <img class='img-fluid' src="${movies[i].img}" alt="Thumbnail">
             <div class="bg-black esconder" id="${i}">
-
-            <p class="infoCard text-white"><a class="text-light pointer" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="modal(${i})" class="pointer"><svg class="me-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-play-circle" viewBox="0 0 16 16">
+            <p class="infoCard text-white"><svg class="me-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-play-circle" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
             <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
-        </svg>${movies[i].title}</a></p>
+        </svg>${movies[i].title}</p>
             </div>
         </div>
     </div>
@@ -25,10 +24,10 @@ function render(a, categ) {
         }
     }
 }
-function addd(a) {
-    let o = (`${a}`);
-    let k = document.getElementById(o)
-    k.classList.add('classenova')
+function adicionarClasse(parametro) {
+    let parametroString = (`${parametro}`);
+    let elementoCapturado = document.getElementById(parametroString)
+    elementoCapturado.classList.add('hoverInfo')
 }
 
 render(growcast, 'growcast')
@@ -47,3 +46,8 @@ function stopVid() {
     let vid = document.getElementById("myVideo");
     vid.setAttribute('src', vid.getAttribute('src'));
 }
+
+let colocarSvg = document.querySelectorAll('.injectSvg')
+colocarSvg.forEach(elementoAreceberSvg => elementoAreceberSvg.innerHTML+=` <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+class="bi bi-caret-right-fill" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"></path></svg>`)
+
