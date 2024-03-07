7ef561f24d62c5990319869520daa3c7a9ef2c50
Sandro Junior <sandrojr998@gmail.com>
2024-03-07 18:23:59 -0300

    ok

--- ModeloAtividade/api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('/var/task/user/pratica01/pratica01.php');
-require('/var/task/user/pratica02/pratica02.php');
-require('/var/task/user/pratica03/pratica03.php');
-
-echo "<a href='/var/task/user/pratica01/pratica01.php'>pratica01</a>";
-echo "<a href='/var/task/user/pratica01/pratica02.php'>pratica02</a>";
-echo "<a href='/var/task/user/pratica03/pratica01.php'>pratica03</a>";
